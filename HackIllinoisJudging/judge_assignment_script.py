1a69e58cdf6a0a629d66ec61ad9792fa56cf68a9
Kyle Begovich <begovichkyle@gmail.com>
2020-03-01 10:32:05 -0600

    fixing bug with off-by-one

--- HackIllinoisJudging/judge_assignment_script.py
+++ HackIllinoisJudging/judge_assignment_script.py
@@ -4,12 +4,12 @@ from math import floor
 
 num_judges = 20
 num_projects = 50
-projects_per_judge = 3
+projects_per_judge = 6
 
 
 judge_assignments = []
-step_size = (num_projects - projects_per_judge) / float(num_judges)
-curr_start = float(0)
+step_size = (num_projects) / float(num_judges)
+curr_start = float(1)
 
 for i in range(num_judges):
     range_start = int(floor(curr_start))
@@ -18,5 +18,10 @@ for i in range(num_judges):
     curr_start += step_size
 
 
-for j in judge_assignments:
-    print(j)
+for i in range(len(judge_assignments)):
+    for j in range(len(judge_assignments[i])):
+        if judge_assignments[i][j] > num_projects:
+            judge_assignments[i][j] -= num_projects
+
+for assign in judge_assignments:
+    print(assign)
